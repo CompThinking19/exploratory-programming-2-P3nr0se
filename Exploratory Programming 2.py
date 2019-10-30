4ac19025d948924ef12a7f36f1e5031e7b7b128e
P3nr0se <55242595+P3nr0se@users.noreply.github.com>
2019-10-30 11:41:58 -0400

    Exploratory 2 Complete
    Finished the assignment as per the instructions. Added a function that counts how many positives there are in a list and called that function twice. Once as a string, and once as a list.

--- Exploratory Programming 2.py
+++ Exploratory Programming 2.py
@@ -1,72 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
-fingers = [-2 ,-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
-# In[4]:
-
-
-print fingers
-
-
-# In[5]:
-
-
-x = len(fingers)
-
-
-# In[6]:
-
-
-print x
-
-
-# In[77]:
-
-
-def positive(list):
-   positive is float in list > 0
-
-
-# In[82]:
-
-
-#This function will run only if there is no negative in the list. I'm notably having trouble creating a function that will create a list of exclusively positive numbers
-
-
-# In[78]:
-
-
-def howpositive(list):
-    y = []
-    for element in list:
-        if (element < 0):
-            raise TypeError("No negativity!")
+#defined how positive function to determine how many positives there are in the list
+def howpositive(numbers):
+    if type(numbers) != list:
+        raise TypeError("Not a list!")
+    positives = 0
     for element in list:
         if (element > 0):
-            print element
-    
-    
-
-
-# In[79]:
-
-
-howpositive(fingers)
-
-
-# In[80]:
-
-
-hand = [1, 2, 3, 4, 5]
-
-
-# In[81]:
-
-
-howpositive(hand)
-
+            positives += 1
+    return positives
+# Calling howpositive with a list
+howpositive([1])
+#Calling howpositive with a string
+howpositive("1")
